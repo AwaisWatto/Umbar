ebdaf13f036a8efba30d73303eddf68a6c94ecff
AwaisWatto <116358085+AwaisWatto@users.noreply.github.com>
2022-11-29 21:11:01 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+
+// import { Route } from 'react-router-dom';
+// import { Routes } from 'react-router-dom';
+
+import './App.css';
+import React from 'react';
+import Example from './Components/Parent';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+
+return (
+   <div>
+      <Example/>
+   </div>
+
+);
+}
+
+export default App;
